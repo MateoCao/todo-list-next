86cdad97143368a30a10bab980e027a14ae634fd
Mateo Cao <mateocao12@gmail.com>
2023-10-05 08:58:15 -0300

    first

---
+++ src/app/(components)/Navbar.js
@@ -0,0 +1,34 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+function Navbar () {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/register')
+  }
+  return (
+        <nav className="h-12 sticky top-0 bg-sky-600 flex font-semibold justify-between items-center p-2">
+            <h1 className="text-2xl text-white">
+              <Link href="/">Casual Chat</Link>
+            </h1>
+            <ul className="flex text-gray-100 hover:text-white justify-around gap-6 items-center">
+              <li>
+                <Link href="/chats">Chats</Link>
+              </li>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <button onClick={handleClick} className="bg-sky-700 p-2 self-center  w-fit rounded hover:bg-sky-800 duration-200">Registrate gratis</button>
+              </li>
+            </ul>
+
+        </nav>
+
+  )
+}
+
+export default Navbar
